212df9e5289cd634e1b9c5a097e03aa5c47a719d
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2018-12-05 08:39:41 +0300

    More cowbell

--- src/Zer0/Helpers/ErrorsAreExceptions.php
+++ src/Zer0/Helpers/ErrorsAreExceptions.php
@@ -8,20 +8,45 @@ namespace Zer0\Helpers;
  */
 class ErrorsAreExceptions
 {
+
+    /**
+     * @var array
+     */
+    private static $listeners = [];
+
+    /**
+     *
+     */
+    public static function addListener(callable $cb): void
+    {
+        self::$listeners[] = $cb;
+    }
+
     /**
      *
      */
     public static function makeItSo(): void
     {
         set_error_handler(function ($severity, $message, $file, $line) {
-            if (!(error_reporting() & $severity)) {
+
+            $exception = new \ErrorException($message, 0, $severity, $file, $line);
+            foreach (self::$listeners as $cb) {
+                $cb($exception);
+            }
+
+            $errLevel = error_reporting();
+            if ($errLevel === 0) {
                 return;
             }
-            throw new \ErrorException($message, 0, $severity, $file, $line);
+            if (!($errLevel & $severity)) {
+                return;
+            }
+            throw $exception;
         });
 
-        assert_options(ASSERT_CALLBACK, function() {
-           throw new \AssertionError;
+        assert_options(ASSERT_CALLBACK, function ($script, $line, $message) {
+            throw new \AssertionError('Failed assertion ' . json_encode($message,
+                    JSON_UNESCAPED_UNICODE) . ' in ' . $script . ':' . $line);
         });
     }
 }
